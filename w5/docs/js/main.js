b7370102ed661d13a8a262ec81605858219ae01d
Wazup2you <wazup2you@gmail.com>
2020-06-23 11:18:26 +0200

    door unlock animation

--- w5/docs/js/main.js
+++ w5/docs/js/main.js
@@ -373,6 +373,43 @@ var Game = (function () {
             if (_this.pigeons.length === 0 && _this.doorsLocked === true) {
                 console.log("Opening doors");
                 _this.doorsLocked = false;
+                this.div = document.createElement("doorN")
+       
+                gameElement.appendChild(this.div)
+    
+                this.div.style.width = "60px"
+                this.div.style.height = "33px"
+                this.div.style.transform = `translate(270px, 0px)`
+    
+                this.div.style.position = "absolute"
+                this.div.style.border = "5px outset blue";
+                this.div = document.createElement("doorE")
+                gameElement.appendChild(this.div)
+    
+                this.div.style.width = "33px"
+                this.div.style.height = "60px"
+                this.div.style.transform = `translate(567px, 270px)`
+    
+                this.div.style.position = "absolute"
+                this.div.style.border = "5px outset blue";
+                this.div = document.createElement("doorS")
+                gameElement.appendChild(this.div)
+    
+                this.div.style.width = "60px"
+                this.div.style.height = "33px"
+                this.div.style.transform = `translate(270px, 567px)`
+    
+                this.div.style.position = "absolute"
+                this.div.style.border = "5px outset blue";
+                this.div = document.createElement("doorW")
+                gameElement.appendChild(this.div)
+    
+                this.div.style.width = "33px"
+                this.div.style.height = "60px"
+                this.div.style.transform = `translate(0px, 270px)`
+    
+                this.div.style.position = "absolute"
+                this.div.style.border = "5px outset blue";
             }
             if (_this.doorsLocked === false) {
                 var background = document.getElementsByTagName("background")[0];
