3e7add8cc912592820dfaac92717bb16dc6ec372
MJ119 <manuja115@gmail.com>
2020-06-24 14:23:04 +0200

    completed narrative

--- narrative/dev/narrative.ts
+++ narrative/dev/narrative.ts
@@ -11,28 +11,28 @@ class Narrative{
         let narrative1 = document.createElement("narrative1")
         this.body.appendChild(narrative1)
 
-        setTimeout(() => this.createNarrative2(), 5000)
+        setTimeout(() => this.createNarrative2(), 10000)
     }
 
     private createNarrative2(){
         let narrative2 = document.createElement("narrative2")
         this.body.appendChild(narrative2)
 
-        setTimeout(() => this.createNarrative3(), 5000)
+        setTimeout(() => this.createNarrative3(), 10000)
     }
 
     private createNarrative3(){
         let narrative3 = document.createElement("narrative3")
         this.body.appendChild(narrative3)
         
-        setTimeout(() => this.createNarrative4(), 5000)
+        setTimeout(() => this.createNarrative4(), 10000)
     }
 
     private createNarrative4(){
         let narrative4 = document.createElement("narrative4")
         this.body.appendChild(narrative4)
         
-        setTimeout(() => this.moveToIndex(), 5000)
+        setTimeout(() => this.moveToIndex(), 10000)
     }
 
     private moveToIndex(){
